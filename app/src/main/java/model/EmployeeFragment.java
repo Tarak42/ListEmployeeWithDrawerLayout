5bc4d729ee1cc78a0addedd69892ef7721bf18f4
Marlon <escobar.marlon@gmail.com>
2019-04-11 19:45:10 -0400

    textView Test

--- app/src/main/java/model/EmployeeFragment.java
+++ app/src/main/java/model/EmployeeFragment.java
@@ -7,10 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.appsmontreal.projectemployeeapp.MainActivity;
 import com.appsmontreal.projectemployeeapp.R;
 
 public class EmployeeFragment extends android.app.Fragment{
-    boolean flag = false;
+    boolean flag;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
@@ -28,14 +29,18 @@ public class EmployeeFragment extends android.app.Fragment{
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Employee employee = (Employee) getArguments().getSerializable("country");
+        Employee employee = (Employee) getArguments().getSerializable(MainActivity.KEY);
 
         if (employee instanceof FullTime){
             FullTime aFullTime = (FullTime) employee;
+            flag = false;
+
             TextView textViewFullTimeId = getActivity().findViewById(R.id.textViewFId);
-            textViewFullTimeId.setText(aFullTime.getEmployeeId());
+            textViewFullTimeId.setText(aFullTime.employeeId);
+            TextView textViewFullTimeName = getActivity().findViewById(R.id.textViewFname);
+            textViewFullTimeName.setText(aFullTime.getEmployeeName());
             TextView textViewFullTimeJob = getActivity().findViewById(R.id.textViewFJob);
-            textViewFullTimeJob.setText(aFullTime.getEmployeeName());
+            textViewFullTimeJob.setText(aFullTime.getEmployeeJob());
             TextView textViewFullTimeStartDate = getActivity().findViewById(R.id.textViewFStart);
             textViewFullTimeStartDate.setText(aFullTime.getStartDate());
             TextView textViewFullTimeSalary = getActivity().findViewById(R.id.textViewFSalary);
@@ -43,13 +48,23 @@ public class EmployeeFragment extends android.app.Fragment{
             Log.i("------>","I'm here");
         }
         else{
+            Contractor aContractor = (Contractor) employee;
+            flag = true;
+
             TextView textViewContractorId = getActivity().findViewById(R.id.textViewCId);
-            TextView textViewContractoreName = getActivity().findViewById(R.id.textViewCName);
+            textViewContractorId.setText(aContractor.employeeId);
+            TextView textViewContractorName = getActivity().findViewById(R.id.textViewCName);
+            textViewContractorName.setText(aContractor.getEmployeeName());
             TextView textViewContractorJob = getActivity().findViewById(R.id.textViewCJob);
+            textViewContractorJob.setText(aContractor.getEmployeeJob());
             TextView textViewContractorStartDate = getActivity().findViewById(R.id.textViewCStart);
+            textViewContractorStartDate.setText(aContractor.getStartDate());
             TextView textViewContractorEndDate = getActivity().findViewById(R.id.textViewCEnd);
+            textViewContractorEndDate.setText(aContractor.getEndDate());
             TextView textViewContractorHoursCost = getActivity().findViewById(R.id.textViewCHoursCost);
+            textViewContractorHoursCost.setText(String.valueOf(aContractor.getHourSalary()));
             TextView textViewContractorENumberHour = getActivity().findViewById(R.id.textViewCNumberHours);
+            textViewContractorENumberHour.setText(String.valueOf(aContractor.getNumberHoursPerWeek()));
         }
 
 //        TextView textViewCapital = getActivity().findViewById(R.id.textViewCapital);
